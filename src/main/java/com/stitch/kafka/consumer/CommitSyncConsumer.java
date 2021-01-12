63361f57b596ee84f5bf5056998695fe14bef523
Administrator <1436037811@qq.com>
2021-01-12 20:00:13 +0800

    finish

--- src/main/java/com/stitch/kafka/consumer/CommitSyncConsumer.java
+++ src/main/java/com/stitch/kafka/consumer/CommitSyncConsumer.java
@@ -6,22 +6,27 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import java.util.Arrays;
 import java.util.Properties;
 
-public class CustomConsumer {
+public class CommitSyncConsumer {
 
     public static void main(String[] args) {
         Properties props = new Properties();
+        //Kafka 集群
         props.put("bootstrap.servers", "150.158.174.69:9092");
+        //消费者组，只要 group.id 相同，就属于同一个消费者组
         props.put("group.id", "test");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
+        props.put("enable.auto.commit", "false");//关闭自动提交 offset
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-        consumer.subscribe(Arrays.asList("second"));
+        consumer.subscribe(Arrays.asList("second"));//消费者订阅主题
         while (true) {
+            //消费者拉取数据
             ConsumerRecords<String, String> records = consumer.poll(100);
-            for (ConsumerRecord<String, String> record : records)
+            for (ConsumerRecord<String, String> record : records) {
                 System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+            //同步提交，当前线程会阻塞直到 offset 提交成功
+            consumer.commitSync();
         }
     }
 
